774ba6a67c5531f73ecd16023136af5b635723a7
EC2 Default User <ec2-user@ip-172-31-38-27.ap-northeast-1.compute.internal>
2020-01-14 17:00:49 +0000

    Complete chapter 1
    Build a basic UI with html and css

---
+++ components/Messages.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="chats-container">
+    <message />
+  </div>
+</template>
+
+<script>
+import Message from './Message.vue'
+
+export default {
+  components: {
+    Message
+  }
+}
+</script>
